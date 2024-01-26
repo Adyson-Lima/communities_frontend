7beb2a802a6a3e7481a0d558d11ff68197e1502e
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-26 10:38:34 -0300

    6° commit - tdd tela Communities: Existe tabela em Communities? RED

--- src/tests/Communities.test.js
+++ src/tests/Communities.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela Communities', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Communities?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
